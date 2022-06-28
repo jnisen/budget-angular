6203e0dcb4defc11aa5947639702f2d8b630b7c0
jnisen <jnisen@gmail.com>
2022-06-28 11:52:47 +0300

    Adding structure in input-expenses'

--- src/app/components/expenses/expenses.component.ts
+++ src/app/components/expenses/expenses.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BudgetService } from 'src/app/services/budget.service';
 
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
-  styleUrls: ['./expenses.component.css']
+  styleUrls: ['./expenses.component.css'],
 })
 export class ExpensesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private _budgetService: BudgetService, private router: Router) {}
 
   ngOnInit(): void {
+    if (this._budgetService.budget === 0) {
+      this.router.navigate(['/budget']);
+    }
   }
-
 }
